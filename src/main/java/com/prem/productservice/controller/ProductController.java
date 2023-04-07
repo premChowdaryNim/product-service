e99bc26ecac29ee3370ab822b747c430c067f7d0
premChowdaryNim <premchowdarynim@gmail.com>
2023-04-07 12:18:26 -0400

    commit

--- src/main/java/com/prem/productservice/controller/ProductController.java
+++ src/main/java/com/prem/productservice/controller/ProductController.java
@@ -16,10 +16,12 @@ import com.prem.productservice.dto.ProductResponse;
 import com.prem.productservice.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/product")
 @RequiredArgsConstructor
+@Slf4j
 public class ProductController {
 
 	private final ProductService productService;
@@ -27,12 +29,16 @@ public class ProductController {
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public void createproduct(@RequestBody ProductRequest productRequest) {
+		log.info("Entered Product Service");
 		 productService.createProduct(productRequest);
+		log.info("Exited Product Service");
+
 	}
 	
 	 @GetMapping
 	 @ResponseStatus(HttpStatus.OK)
 	 public List<ProductResponse> getAllProducts() {
+		log.info("Entered Product Service");
 	      return productService.getAllProducts();
 	 }
 }
